27f1c4f318c7d95641da73e8372fad2683b43916
Peter S. <30744382+espaceau@users.noreply.github.com>
2017-08-19 16:54:05 +1000

    Minor Change

--- docker-remote-cmd-nodejs/server.js
+++ docker-remote-cmd-nodejs/server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Remote cmd API is alive. Use /exec/<commandToBeExecuted> \n");
+  res.send("The remote cmd API is alive. Use /exec/<commandToBeExecuted> \n");
 });
 
 app.get('/exec/:cmd', (req, res) => {
